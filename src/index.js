bb7e86d82db3139a9a71be29613ceddf95f4dff0
OlenaPobehailo <olena.pobehailo@gmail.com>
2023-11-05 12:09:15 +0200

    delete storing  contacts in local storage

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 import { ToastContainer } from 'react-toastify';
 import { App } from 'components/App';
-import { persistor, store } from 'redux/store';
+import { store } from 'redux/store';
 import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
       <ToastContainer
         className="Toastify__toast-body"
         position="top-left"
